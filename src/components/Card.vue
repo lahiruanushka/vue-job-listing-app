e74908f79b409172cc427501107f5b283552d3ab
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-18 22:32:07 +0530

    Create Card.vue

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  defineProps({
+    bg: {
+      type: String,
+      default: 'bg-white' // Default background color
+    }
+  });
+</script>
+
+<style scoped>
+/* Add any scoped styles here */
+</style>
