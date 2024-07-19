e4f621d7c1439e2458a49ef48abccabf9e2d94a9
Lahiru Anushka <121139285+lahiruanushka@users.noreply.github.com>
2024-07-19 10:42:18 +0530

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,22 @@
 <script setup>
-import logo from '@/assets/img/logo.png'
+import logo from '@/assets/img/logo.png';
 </script>
 
 <template>
-  <nav class="bg-blue-700 border-b border-blue-500">
+  <nav class="bg-white border-b border-gray-200">
     <div class="container mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <!-- Logo -->
         <a class="flex items-center space-x-2" href="index.html">
-          <img class="h-10 w-auto" :src="job-search" alt="Vue Jobs" />
-          <span class="hidden md:block text-white text-2xl font-bold">Jobs Finder</span>
+          <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+          <span class="hidden md:block text-gray-800 text-2xl font-bold">Jobs Finder</span>
         </a>
         <!-- Navigation Links -->
         <div class="md:flex md:items-center">
           <div class="flex space-x-4">
-            <a href="index.html" class="text-white bg-blue-900 hover:bg-blue-800 rounded-md px-3 py-2 transition duration-300">Home</a>
-            <a href="jobs.html" class="text-white hover:bg-blue-800 rounded-md px-3 py-2 transition duration-300">Jobs</a>
-            <a href="add-job.html" class="text-white hover:bg-blue-800 rounded-md px-3 py-2 transition duration-300">Add Job</a>
+            <a href="/" class="text-gray-800 hover:bg-gray-100 rounded-md px-3 py-2 transition duration-300">Home</a>
+            <a href="/jobs" class="text-gray-800 hover:bg-gray-100 rounded-md px-3 py-2 transition duration-300">Jobs</a>
+            <a href="/jobs/add" class="text-gray-800 hover:bg-gray-100 rounded-md px-3 py-2 transition duration-300">Add Job</a>
           </div>
         </div>
       </div>
